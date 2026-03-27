64b7945f1fe389cbb7b1006bedcc21e8f2596212
wdchenxyz <wdchenxyz@gmail.com>
2026-03-27 15:04:41 +0800

    feat: register Image in tmux component registry

--- src/catalog.ts
+++ src/catalog.ts
@@ -2,10 +2,12 @@ import { standardComponents, type ComponentRegistry } from "@json-render/ink";
 import { LineChart } from "./components/line-chart.js";
 import { Histogram } from "./components/histogram.js";
 import { Heatmap } from "./components/heatmap.js";
+import { Image } from "./components/image.js";
 
 export const registry: ComponentRegistry = {
   ...standardComponents,
   LineChart,
   Histogram,
   Heatmap,
+  Image,
 };
